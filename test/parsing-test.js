3c3cf087249e2107383e7c2282d171713a638bca
cqx931 <cqx931@gmail.com>
2020-05-06 17:30:27 -0400

    parse new syntags

--- test/parsing-test.js
+++ test/parsing-test.js
@@ -1,3 +1,13 @@
+// Definitions
+const CLAUSE_BREAKS = ["_,_", "_;_", "_:_", "</cb>"];
+const THOUGHT_BREAKS = ["_._", "_?_", "_!_", "</tb>"]; // Done
+const PARAGRAPH_BREAK = "<pb/>"; // Done
+const SECTION_BREAK = "<sb/>"; // Done
+const UNIT_PAIRS = ["<ub>","</ub>"];// Done
+
+const DEFAULT_PATH = ".tb:last";
+
+// Tools
 function readTextFile(file, callback)
 {
     var rawFile = new XMLHttpRequest();
@@ -16,71 +26,178 @@ function readTextFile(file, callback)
     rawFile.send(null);
 }
 
-function createNewPage(index) {
-  const page = $('#template .page').clone()[0];
-  page.id = "page" + index;
-  $('#content').append(page);
+function endsWithAny(str, array){
+  for (let i = 0; i < array.length; i++) {
+    const word = array[i];
+    if (str.endsWith(word)) return true;
+  }
+  return false;
+}
+// Parsing Section
+function removeGitDiffSyntags(content) {
+  return content.replace(/(^-|^\+|_)/g,"");
+}
+
+function removeBreaks(content) {
+  return content.replace(/(<ub>|<\/ub>|<sb\/>|<pb\/>)/g,"");
+}
+
+function removeEmptyElements(selector) {
+  // remove redundant
+  $(selector).each(function() {
+     if( $(this)[0].innerHTML == "")  $(this)[0].remove();
+  });
+}
+
+function getCurrentIndexInDerectParent(dom) {
+  const children = dom.parent().children();
+  for (var i = 0; i < children.length; i++) {
+    if (children[i] == dom[0]) return i;
+  }
+  return false;
 }
 
 function parseText(data) {
-  let a_file = [], b_file = [];
-  const lines = data.split("\n")
 
+  const lines = data.split("\n");
   // skip the top section
   lines.splice(0, 5);
+  let contentToBeAppend = document.createElement('div');
+  $(contentToBeAppend).attr("id", "content");
 
-  let currentPage = 1, currentNo = 0, unit = false;
-  createNewPage(currentPage);
+  let currentPage = 1, currentNo = 0, match = false;
+
+  createNewPage(currentPage, contentToBeAppend);
 
   for (var i = 0; i < lines.length; i++) {
 
-    const line = lines[i],
-          type = line[0];
+    const line = lines[i].substr(1),
+          type = lines[i][0];
     let content = line,
         newSpan =  document.createElement("span");
-    // remove syntags
-    content = content.replace(/(^-|^\+|_)/g,"");
-    content = content.replace(/(<ub>|<\/ub>|<sb\/>|<pb\/>)/g,"");
+
+    // clean up syntags
+    // content = content.replace(/_(,|;|:|.|\?|!)_/g,"$1 ");
+    content = removeGitDiffSyntags(content);
+    content = removeBreaks(content);
 
     newSpan.innerText = content;
+
+    const currentAdiv = $(contentToBeAppend).find('#page' + currentPage +' .adiv'),
+          currentBdiv =  $(contentToBeAppend).find('#page' + currentPage +' .bdiv');
 
     switch(type) {
       case "-":
          newSpan.id = "a" + currentNo;
-         unit = true;
+         match = true;
          newSpan.classList += " hide";
-         $('#page' + currentPage +' .adiv').append(newSpan);
          // hide = append a span (.hide)
+         content != "" && currentAdiv.find(DEFAULT_PATH).append(newSpan);
         break;
       case " ":
-          if (unit == true) currentNo++;
-          unit = false;
-          if (line ==" _<pb/>_") {
+          if (match == true) currentNo++;
+          match = false;
+          if (line == SECTION_BREAK) {
+            // Handle section breaks
             currentPage ++;
-            createNewPage(currentPage);
-          } else {
-            $('#page' + currentPage +' .adiv').append(newSpan);
-            // unchanged  = append a span
-          }
+            createNewPage(currentPage, contentToBeAppend);
+          } else if (line == PARAGRAPH_BREAK) {
+            // Handle paragraph breaks
+            currentAdiv.append("<p></p>");
+            currentBdiv.append("<p></p>");
+          } else if (UNIT_PAIRS.indexOf(line) > -1) {
+            // Handle unit
+            if (line == UNIT_PAIRS[0]) {
+              const unit = "<span class='unit manual'><span class='tb'></span></span>";
+              currentAdiv.find("p:last").append(unit);
+              currentBdiv.find("p:last").append(unit);
+            } else {
+              const tb = "<span class='tb'></span>";
+              currentAdiv.find("p:last").append(tb);
+              currentBdiv.find("p:last").append(tb);
+            }
 
+          } else {
+            // shared  = append to both a span & b span
+            newSpan.classList += " shared";
+            if (content != "") {
+              currentAdiv.find(DEFAULT_PATH).append(newSpan);
+              currentBdiv.find(DEFAULT_PATH).append(newSpan.cloneNode(true));
+            }
+          }
         break;
       case "+":
           newSpan.id = "b" + currentNo;
-          unit = false;
+          match = false;
           currentNo++;
-          $('#page' + currentPage +' .bdiv').append(newSpan);
           // add = append b span (display:none)
+          content != "" && currentBdiv.find(DEFAULT_PATH).append(newSpan);
         break;
       case "~":
-        // new line
-        if (unit == true) currentNo++;
-        unit = false;
+        // new line : no visual representation in the html
+        if (match == true) currentNo++;
+        match = false;
         break;
       default :
         //console.log("[Warning] Unparsable line", line);
+    } // End of Switch
+
+    // Handle THOUGHT_BREAKS
+    if(endsWithAny(line, THOUGHT_BREAKS)) {
+      const tb = "<span class='tb'></span>";
+      currentAdiv.find(".tb:last").parent().append(tb);
+      currentBdiv.find(".tb:last").parent().append(tb);
     }
 
-  }
+  } // End of for loop
+  // append content
+  $('body').append(contentToBeAppend);
+  removeEmptyElements('.tb')
+  // set current page
+  $('#page1').addClass('current')
+
+  $('.adiv > p > span').mouseover(function() {
+    const spanIdx = getCurrentIndexInDerectParent($(this));
+    const pIdx = getCurrentIndexInDerectParent($(this).parent());
+    const currentPage = $(this).parent().parent().parent();
+
+    const bspan = currentPage.find('.bdiv').children().eq(pIdx).children().eq(spanIdx);
+    $('.bdiv p > span').hide();
+    bspan.show();
+  })
+
+  $('.adiv > p > span').mouseout(function() {
+    $('.bdiv p > span').hide();
+  })
+  $('.menu li').click(function() {
+    console.log(this.innerText)
+    $('.page').removeClass('current')
+    $('#page'+ this.innerText).addClass('current')
+  })
+
 }
+// End of Parsing Section
+
+// Visualization Section
+function createNewPage(index, wrapper) {
+  const page = $('#template .page').clone()[0];
+  page.id = "page" + index;
+  $(wrapper).append(page);
+  // populate menu
+  $('.menu ul').append("<li>"+ index + "</li>");
+}
+
+function getClosestSharedSpan() {
+  // TODO
+  // return closest;
+}
+
+function repositionB(element, anchor) {
+  // TODO
+  // KEY: get offsetLeft
+  // Element defines which level we are working with: it can be <cb> or <ub>
+}
+
+// End of Visualization Section
 
 readTextFile("../data/ab_worddiff.txt", parseText);
