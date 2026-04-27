ca8458b2f3219ed1e02d2d925f8016da15101fc7
John Cayley <John_Cayley@brown.edu>
2026-04-26 22:36:39 -0400

    get dev_john

---
+++ NotNeededForDeployment/jsDross.js
@@ -0,0 +1,15 @@
+
+
+// if (i < diffs.length - 2) {
+    //   oneSpace = getType(diffs[i+1]) == " " && diffs[i+1].value == " ";
+    //   span = span + " ";
+    //   i++;
+    //   let nextType = getType(diffs[i+1]);
+    //   if (oneSpace && nextType == lastType) {
+    //     span = span + diffs[i+1].value;
+    //     continue;
+    //   }
+    //   else lastType = nextType;
+    // }
+
+
